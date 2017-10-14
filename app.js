1b55b9c0ccf1895207c4f1bcd3df0d7e0159700f
Juri Adams <juri@4da.ms>
2017-10-14 23:50:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ client_twitch.on("chat", (channel, user, message, self) => {
 
     if(message.toLowerCase().includes("!elo")) {
 
-      api.get('euw1', 'league.getAllLeaguePositionsForSummoner', '95457313')
+      api.get('euw1', 'league.getAllLeaguePositionsForSummoner', '95457313') // ID of Summoner "Yu RedPhoenix"
         .then(data => {
-          let entry = data.find(e => e.queueType === 'RANKED_SOLO_5x5');
+          let entry = data.find(e => e.queueType === 'RANKED_SOLO_5x5'); // Queue Type
           client_twitch.action('#followredphoenix', '[🤖] ' + entry.playerOrTeamName + ' ist momentan ' + entry.tier + ' ' + entry.rank + ' mit ' + entry.leaguePoints + ' LP. Schreibe "!winrate" in den Chat, für mehr Informationen!');
       });
 
@@ -63,15 +63,16 @@ client_twitch.on("chat", (channel, user, message, self) => {
 
     if(message.toLowerCase().includes("!winrate")) {
 
-      api.get('euw1', 'league.getAllLeaguePositionsForSummoner', '95457313')
+      api.get('euw1', 'league.getAllLeaguePositionsForSummoner', '95457313') // ID of Summoner "Yu RedPhoenix"
       .then(data => {
-        let entry = data.find(e => e.queueType === 'RANKED_SOLO_5x5');
-
+        let entry = data.find(e => e.queueType === 'RANKED_SOLO_5x5'); // Queue Type
+        
+        // Calculating the winrate... Split up to simplify
         var q_wins = entry.wins
         var q_losses = entry.losses
 
         var q_winrate = q_wins / (q_wins + q_losses) * 100
-        var q_winrate_rounded = Math.round(q_winrate * 100) / 100
+        var q_winrate_rounded = Math.round(q_winrate * 100) / 100 // Rounding up winrate
 
         client_twitch.action("#followredphoenix", "[🤖] Yu RedPhoenix hat eine Winrate von " + q_winrate_rounded + "%. (" + q_wins + " W / " + q_losses + " L)")
 
