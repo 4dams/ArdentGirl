2b22e2d83e10dce344d9f29574c3c3e1c193fbde
Juri Adams <juri@4da.ms>
2017-10-15 02:54:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -42,42 +42,49 @@ client_twitch.on("chat", (channel, user, message, self) => {
 
   if(channel == "#followredphoenix") {
 
+    // Elo Command
     if(message.toLowerCase().includes("!elo")) {
-
-      api.get('euw1', 'league.getAllLeaguePositionsForSummoner', '95457313') // ID of Summoner "Yu RedPhoenix"
+      api.get('euw1', 'league.getAllLeaguePositionsForSummoner', '95457313')
         .then(data => {
-          let entry = data.find(e => e.queueType === 'RANKED_SOLO_5x5'); // Queue Type
+          let entry = data.find(e => e.queueType === 'RANKED_SOLO_5x5');
           client_twitch.action('#followredphoenix', '[🤖] ' + entry.playerOrTeamName + ' ist momentan ' + entry.tier + ' ' + entry.rank + ' mit ' + entry.leaguePoints + ' LP. Schreibe "!winrate" in den Chat, für mehr Informationen!');
       });
-
       console.log("[" + moment().format('LTS') + "] Elo requested in " + channel + "!")
     }
 
+    // Summoner ID
     if(message.toLowerCase().includes("!summoner")) {
-
       api.get('euw1', 'summoner.getBySummonerName', 'YuRedPhoenix')
         .then(data => client_twitch.action("#followredphoenix", "[🤖] Die Summoner ID von " + data.name + " lautet " + data.id + '.'));
-
-      console.log("[" + moment().format('LTS') + "] Summoner requested in " + channel + "!")
     }
 
+    // Winrate Command
     if(message.toLowerCase().includes("!winrate")) {
-
-      api.get('euw1', 'league.getAllLeaguePositionsForSummoner', '95457313') // ID of Summoner "Yu RedPhoenix"
+      api.get('euw1', 'league.getAllLeaguePositionsForSummoner', '95457313')
       .then(data => {
-        let entry = data.find(e => e.queueType === 'RANKED_SOLO_5x5'); // Queue Type
-        
-        // Calculating the winrate... Split up to simplify
+        let entry = data.find(e => e.queueType === 'RANKED_SOLO_5x5');
         var q_wins = entry.wins
         var q_losses = entry.losses
-
         var q_winrate = q_wins / (q_wins + q_losses) * 100
-        var q_winrate_rounded = Math.round(q_winrate * 100) / 100 // Rounding up winrate
-
+        var q_winrate_rounded = Math.round(q_winrate * 100) / 100
         client_twitch.action("#followredphoenix", "[🤖] Yu RedPhoenix hat eine Winrate von " + q_winrate_rounded + "%. (" + q_wins + " W / " + q_losses + " L)")
-
       });
+    }
 
+    // Top Champion Command - Broken
+    if(message.toLowerCase().includes("!topchamp")) {
+      api.get('euw1', 'championMastery.getAllChampionMasteries', '95457313')
+      .then(data => {
+        let entry2 = data.find(e2 => e2.championId === '40');
+        var champ1_points = entry2.championPoints
+        var champ1_level = entry2.championLevel
+        client_twitch.action("#followredphoenix", "[🤖] Yu RedPhoenix' bester Champion ist Janna mit Championlevel " + champ1_level + " und " + champ1_points + " Masterypoints.")
+      });
+    }
+
+    // Command List Command
+    if(message.toLowerCase().includes("!commands")) {
+      client_twitch.action("#followredphoenix", "[🤖] Verfügbare Commands: !elo, !winrate, !topchamp, !commands.")
     }
 
   }
